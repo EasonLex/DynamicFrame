4502b25c33e5ae17a8933f60ed0adc9d83d96468
EasonLex <easonlex@gmail.com>
2016-04-11 16:37:53 +0800

    Create UILabel+DynamicFrame.m

---
+++ UILabel+DynamicFrame.m
@@ -0,0 +1,47 @@
+//
+//  UILabel+DynamicFrame.m
+//  EasonLex
+//
+//  Created by Eason Lin on 2015/12/29.
+//  Copyright © 2015年 outsm. All rights reserved.
+//
+
+#import "UILabel+DynamicFrame.h"
+
+@implementation UILabel (DynamicFrame)
+
+- (instancetype)initWithDynamicHeight:(CGRect)frame withFont:(UIFont *)font withText:(NSString *)text
+{
+    self = [super init];
+    if (self) {
+        [self setFont:font];
+        [self setNumberOfLines:0];
+        [self setText:text];
+        
+        NSDictionary *attributes = @{NSFontAttributeName: font};
+        
+        CGRect descFrame = [text boundingRectWithSize:CGSizeMake(frame.size.width, 999) options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil];
+        
+        [self setFrame:CGRectMake(frame.origin.x, frame.origin.y, descFrame.size.width, descFrame.size.height)];
+    }
+    return self;
+}
+
+- (instancetype)initWithDynamicWidth:(CGRect)frame withFont:(UIFont *)font withText:(NSString *)text
+{
+    self = [super init];
+    if (self) {
+        [self setFont:font];
+        [self setNumberOfLines:0];
+        [self setText:text];
+        
+        NSDictionary *attributes = @{NSFontAttributeName: font};
+        
+        CGRect descFrame = [text boundingRectWithSize:CGSizeMake(999, frame.size.height) options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil];
+        
+        [self setFrame:CGRectMake(frame.origin.x, frame.origin.y, descFrame.size.width, descFrame.size.height)];
+    }
+    return self;
+}
+
+@end
